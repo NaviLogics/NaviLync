7c208e056eab8956f727c0f4557da1d306569e4d
NaviLogics <132571220+NaviLogics@users.noreply.github.com>
2026-09-19 17:45:56 +0300

    feat: restore NaviLync localization foundation and portable build

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,7 @@ import { runMigrations } from '@/utils/migrations'
 
 import App from './App.vue'
 import { contextMenu } from './directives/contextMenu'
+import i18n from './plugins/i18n'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
@@ -81,7 +82,7 @@ if (!import.meta.env.DEV) {
 app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.component('VueDraggableResizable', VueDraggableResizable)
 app.directive('contextmenu', contextMenu)
-app.use(router).use(vuetify).use(createPinia()).use(FloatingVue).use(VueVirtualScroller)
+app.use(router).use(vuetify).use(createPinia()).use(i18n).use(FloatingVue).use(VueVirtualScroller)
 app.mount('#app')
 
 // Initialize the logger store
